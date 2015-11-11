c90a74f358eaef46a1e4d11886224c66c53aeefa
Koehn, Brad <Brad.Koehn@bluestembrands.com>
2015-11-11 14:02:36 -0600

    getting started

---
+++ docker/run_as_diaspora.sh
@@ -0,0 +1,11 @@
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 && \
+  curl -L dspr.tk/1t | bash && \
+  echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"' >> ~/.bashrc && \
+  source "$HOME/.rvm/scripts/rvm" && \
+  rvm autolibs read-fail && \
+  rvm install 2.1 && \
+  cd ~ && \
+  git clone -b master git://github.com/diaspora/diaspora.git && \
+  cd diaspora && \
+  cp /tmp/database.yml config/database.yml
+  cp /tmp/diaspora.yml config/diaspora.yml
