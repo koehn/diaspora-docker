11627d9f25b9a106692391dde51f0596fa959b85
Koehn, Brad <Brad.Koehn@bluestembrands.com>
2015-11-17 14:48:12 -0600

    added commands to manage diaspora pod from outside docker

--- docker/diaspora/run_as_diaspora.sh
+++ docker/diaspora/run_as_diaspora.sh
@@ -7,5 +7,7 @@ gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB8
   cd ~ && \
   git clone -b master git://github.com/diaspora/diaspora.git && \
   cd diaspora && \
-  cp /tmp/database.yml config/database.yml
-  cp /tmp/diaspora.yml config/diaspora.yml
+  cp /tmp/database.yml config/database.yml && \
+  cp /tmp/diaspora.yml config/diaspora.yml && \
+  gem install bundler && \
+  RAILS_ENV=production DB=postgres  bin/bundle install --without test development
