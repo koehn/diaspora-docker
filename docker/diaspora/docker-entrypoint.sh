751e31a9b22ecb9cb7989ecae7500164c5101a0c
Koehn, Brad <Brad.Koehn@bluestembrands.com>
2015-11-17 15:44:29 -0600

    getting the diaspora container working

--- docker/diaspora/docker-entrypoint.sh
+++ docker/diaspora/docker-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ "$1" = 'run' ]; then
-	su - diaspora -c "cd diaspora && DB=production RAILS_ENV=production ./script/server"
+	su - diaspora -c "cd /home/diaspora/diaspora && DB=production RAILS_ENV=production ./script/server"
 elif [ "$1" = 'init-db' ]; then
-	su - diaspora -c "RAILS_ENV=production DB=postgres  bin/rake db:create db:schema:load"
-elif [ "$1" = 'precompile-assets']; then
-	su - diaspora -c "RAILS_ENV=production DB=postgres bin/rake assets:precompile"
+	su - diaspora -c "cd /home/diaspora/diaspora && RAILS_ENV=production DB=postgres  bin/rake db:create db:schema:load"
+elif [ "$1" = 'precompile-assets' ]; then
+	su - diaspora -c "cd /home/diaspora/diaspora && RAILS_ENV=production DB=postgres bin/rake assets:precompile"
 fi
