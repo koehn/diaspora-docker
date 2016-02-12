e76d19f1d0280fc92b4d29900dd6ed5d46c8568f
Koehn, Brad <Brad.Koehn@bluestembrands.com>
2016-02-12 14:38:18 -0600

    made postgres finally work with an external disk image. boom

--- docker/database/docker-db-entrypoint.sh
+++ docker/database/docker-db-entrypoint.sh
@@ -1,6 +1,6 @@
 export IMAGE_FILE=${IMAGE_FILE:-/var/cache/postgres/databaseimage.img}
 export MOUNTPOINT=${MOUNTPOINT:-/var/lib/postgresql}
-export SIZE=${SIZE:-800M}
+export SIZE=${SIZE:-80M}
 
 echo IMAGE_FILE: $IMAGE_FILE
 echo MOUNTPOINT: $MOUNTPOINT
@@ -15,6 +15,7 @@ if [ ! -f $IMAGE_FILE ]; then \
     dd if=/dev/zero of=$IMAGE_FILE bs=800M seek=100 count=0 && \
     mkfs -t ext4 $IMAGE_FILE ; fi
 
-mkdir -p $MOUNTPOINT && mount -t auto -o loop $IMAGE_FILE $MOUNTPOINT
+rm -rf $MOUNTPOINT/*
 
-/docker-entrypoint.sh postgres
+mkdir -p $MOUNTPOINT && mount -t auto -o loop $IMAGE_FILE $MOUNTPOINT && \
+  /docker-entrypoint.sh postgres
--- docker/diaspora/docker-entrypoint.sh
+++ docker/diaspora/docker-entrypoint.sh
@@ -5,18 +5,7 @@ function do_as_diaspora {
 }
 
 function setup {
-	su - diaspora -c "cd /home/diaspora && \
-	gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 && \
-	  curl -L dspr.tk/1t | bash && \
-	  echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"' >> ~/.bashrc && \
-	  source "$HOME/.rvm/scripts/rvm" && \
-	  rvm autolibs read-fail && \
-	  rvm install 2.1 && \
-	  cd ~ && \
-	  git clone -b master git://github.com/diaspora/diaspora.git && \
-	  cd diaspora && \
-	  gem install bundler && \
-	  RAILS_ENV=production DB=postgres  bin/bundle install --without test development "
+	su - diaspora -c "/run_as_diaspora.sh"
 }
 
 function run {
