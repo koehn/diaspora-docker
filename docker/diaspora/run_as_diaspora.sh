0e9db69e87b5ef40e8e91d8edb1421db85419914
Koehn, Brad <Brad.Koehn@bluestembrands.com>
2016-02-15 15:04:32 -0600

    moved diaspora home to container

--- docker/diaspora/run_as_diaspora.sh
+++ docker/diaspora/run_as_diaspora.sh
@@ -1,16 +1,20 @@
 #!/bin/bash --login
-cd /home/diaspora && \
+
+export HOME=/home/diaspora
+
+cd $HOME && \
   gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 && \
   curl -L dspr.tk/1t | bash && \
-  echo '[[ -s "/home/diaspora/.rvm/scripts/rvm" ]] && source "/home/diaspora/.rvm/scripts/rvm"' >> ~/.bashrc && \
+  echo '[[ -s "/home/diaspora/.rvm/scripts/rvm" ]] && source "/home/diaspora/.rvm/scripts/rvm"' >> /home/diaspora/.bashrc && \
   source "/home/diaspora/.rvm/scripts/rvm" && \
   rvm autolibs read-fail && \
   rvm install 2.1 && \
   cd ~ && \
-  git clone -b master git://github.com/diaspora/diaspora.git && \
+  git init && \
+  git remote add origin git://github.com/diaspora/diaspora.git && \
+  git fetch && \
+  git checkout -t origin/master && \
   cd diaspora && \
-  ln -s remote-config/database.yml config/database.yml && \
-  ln -s remote-config/diaspora.yml config/diaspora.yml && \
   gem install bundler && \
   mkdir /tmp/.eye && ln -s /tmp/.eye /home/diaspora/.eye && \
   RAILS_ENV=production DB=postgres  bin/bundle install --without test development
