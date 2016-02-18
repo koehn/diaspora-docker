fd3e7a090c70c328dfe92d6a59f7a702d2ebd307
Brad Koehn <brad@koehn.com>
2016-02-18 12:45:12 -0600

    made diaspora pod creation, database update, etc actually work

--- docker/diaspora/run_as_diaspora.sh
+++ docker/diaspora/run_as_diaspora.sh
@@ -5,16 +5,15 @@ export HOME=/home/diaspora
 cd $HOME && \
   gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 && \
   curl -L dspr.tk/1t | bash && \
-  echo '[[ -s "/home/diaspora/.rvm/scripts/rvm" ]] && source "/home/diaspora/.rvm/scripts/rvm"' >> /home/diaspora/.bashrc && \
+  echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"' >> $HOME/.bashrc && \
   source "/home/diaspora/.rvm/scripts/rvm" && \
   rvm autolibs read-fail && \
   rvm install 2.1 && \
-  cd ~ && \
+  cd $HOME/diaspora && \
   git init && \
   git remote add origin git://github.com/diaspora/diaspora.git && \
   git fetch && \
   git checkout -t origin/master && \
-  cd diaspora && \
   gem install bundler && \
-  mkdir /tmp/.eye && ln -s /tmp/.eye /home/diaspora/.eye && \
   RAILS_ENV=production DB=postgres  bin/bundle install --without test development
+
