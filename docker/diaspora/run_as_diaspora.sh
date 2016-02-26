5602d72eac2f4c8896e8c9854aaa9537c0004b75
Brad Koehn <brad@koehn.com>
2016-02-25 18:51:51 -0600

    moved back to compose volumes; need to work out how to get the configs into diaspora from host

--- docker/diaspora/run_as_diaspora.sh
+++ docker/diaspora/run_as_diaspora.sh
@@ -1,7 +1,5 @@
 #!/bin/bash --login
 
-export HOME=/home/diaspora
-
 cd $HOME && \
   gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 && \
   curl -L dspr.tk/1t | bash && \
@@ -9,11 +7,7 @@ cd $HOME && \
   source "/home/diaspora/.rvm/scripts/rvm" && \
   rvm autolibs read-fail && \
   rvm install 2.1 && \
-  cd $HOME/diaspora && \
-  git init && \
-  git remote add origin git://github.com/diaspora/diaspora.git && \
-  git fetch && \
-  git checkout -t origin/master && \
+  git clone git://github.com/diaspora/diaspora.git && \
+  cd diaspora && \
   gem install bundler && \
   RAILS_ENV=production DB=postgres  bin/bundle install --without test development
-
