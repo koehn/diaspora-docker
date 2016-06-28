f3cd526e5bc144789c1020206feb54f350e9ef52
Koehn, Brad <Brad.Koehn@bluestembrands.com>
2016-06-28 16:10:29 -0500

    making it all work with a shared directory for diaspora code

--- docker/diaspora/run_as_diaspora.sh
+++ docker/diaspora/run_as_diaspora.sh
@@ -1,8 +1,7 @@
 #!/bin/bash --login
 
 cd $HOME && \
-  gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 && \
-  curl -L dspr.tk/1t | bash && \
+  curl -L https://s.diaspora.software/1t | bash && \
   echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"' >> $HOME/.bashrc && \
   source "/home/diaspora/.rvm/scripts/rvm" && \
   rvm autolibs read-fail && \
