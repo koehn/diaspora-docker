851ceb9de726f2002b4a5ceedd3bfde7bf374e86
mprov <mprov@dolores.ross154>
2018-09-12 17:45:25 -0700

    build-time errors fixed: - `curl -L dspr.tk/1t` replaced with `curl -sSL https://s.diaspora.software/1t` - `libgmp-dev` added to apt-get install list. - rvm instructed to install ruby-2.4.4 instead of 2.1

--- docker/diaspora/run_as_diaspora.sh
+++ docker/diaspora/run_as_diaspora.sh
@@ -1,9 +1,9 @@
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 && \
-  curl -L dspr.tk/1t | bash && \
+  curl -sSL https://s.diaspora.software/1t | bash && \
   echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"' >> ~/.bashrc && \
   source "$HOME/.rvm/scripts/rvm" && \
   rvm autolibs read-fail && \
-  rvm install 2.1 && \
+  rvm install ruby-2.4.4 && \
   cd ~ && \
   git clone -b master git://github.com/diaspora/diaspora.git && \
   cd diaspora && \
